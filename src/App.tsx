7a4b4abbcc140ffa5310fdcd4281f8b0c943bfce
anilcanboga <anlcn.boga@hotmail.com>
2025-08-16 14:31:08 +0300

    react+ts+tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="p-4 text-3xl h-screen flex justify-center items-center">
+      React + TS + Tailwind Starter Pack
+    </div>
+  );
+};
+
+export default App;
